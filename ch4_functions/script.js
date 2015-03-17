ec93affcef7190e14349cfd827a39c466bdafe11
Brian Wynn <apple.com>
2015-03-17 13:42:46 -0700

    Added examples from the arguments, retun, exceptions, and augmenting types section of chapter 4.

--- ch4_functions/script.js
+++ ch4_functions/script.js
@@ -40,7 +40,7 @@ myObject.double = function() {
 // invoke double as a method
 
 myObject.double();
-document.writeln(myObject.value);
+//document.writeln(myObject.value);
 
 // Create a constructor function called Quo
 // it makes an object with a status property
@@ -59,4 +59,90 @@ Quo.prototype.get_status = function() {
 // make an instance of quo
 
 var myQuo = new Quo('confused');
-document.writeln(myQuo.get_status());   // confused
+//document.writeln(myQuo.get_status());   // confused
+
+// make an array of 2 numbers and then add them
+
+var array = [3, 4];
+var sum = add.apply(null, array);
+
+// make an object with a status member
+
+var statusObject = {
+  status: 'A-OK'
+};
+
+// statusObject does not inherit from Quo.prototype,
+// but we can invoke the get_status method on
+// statusObject even though statusObject does not have
+// a get_status method.
+
+var status = Quo.prototype.get_status.apply(statusObject);    // status is 'a-ok'
+
+// make a function that adds a lot of stuff
+
+// note that definining the variable sum inside of
+// the function does not interfere with the sum
+// defined outside of the funciton. The function
+// only sees the inner one
+
+var sum = function() {
+  var i, sum = 0;
+  for (i = 0; i < arguments.length; i += 1) {
+    sum += arguments[i];
+  }
+  return sum;
+};
+
+//document.writeln(sum(4,8,15,16,23,42)); // 108
+
+var add = function(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw {
+      name: 'TypeError',
+      message: 'add needs numbers'
+    };
+  }
+  return a + b;
+}
+
+// make a try_it function that calls the new add
+// function incorrectly
+
+var try_it = function() {
+  try {
+    add('seven');
+  } catch (e) {
+    document.writeln(e.name + ': ' + e.message);
+  }
+}
+
+//try_it();       // call function
+
+// augmenting types
+Function.prototype.method = function (name, func) {
+  this.prototype[name] = func;
+  return this;
+}
+
+Number.method('integer', function() {
+  return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
+//document.writeln((-10/3).integer()); // -3
+
+//regex
+String.method('trim', function() {
+  return this.replace(/^\s+|\s+$/g, '');
+});
+
+//document.writeln('"' + " neat ".trim() + '"');
+
+// add a method conditionally - this protects the public nature of prototypes
+
+Function.prototype.method = function(name, func) {
+  if (!this.prototype[name]) {
+    this.prototype[name] = func;
+    return this;
+  }
+};
