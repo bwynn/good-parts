78a27daa64d065bfc8ca64392f5363dc158587a2
Brian Wynn <apple.com>
2015-03-23 16:52:06 -0700

    Adding an example of psuedoclassical inheritance using prototype constructors

---
+++ ch5_inheritance/script.js
@@ -0,0 +1,47 @@
+var Mammal = function (name) {
+  this.name = name;
+};
+
+Mammal.prototype.get_name = function() {
+  return this.name;
+};
+
+Mammal.prototype.says = function() {
+  return this.saying || '';
+};
+
+var myMammal = new Mammal('Herb the Mammal');
+var name = myMammal.get_name(); // 'Herb the Mammal'
+
+// make another pseudoclass that inherits mammal by defining it's constructor
+// function and replacing its prototype with an instance of mammal
+
+var Cat = function (name) {
+  this.name =  name;
+  this.saying = 'meow';
+};
+
+// replace Cat.prototype with a new instance of mammal
+
+Cat.prototype = new Mammal();
+// augment the new prototype with
+// purr and get_name methods
+
+Cat.prototype.purr = function(n) {
+  var i, s = '';
+  for (i = 0; i < n; i += 1) {
+    if (s) {
+      s += '-';
+    }
+    s += 'r';
+  }
+  return s;
+};
+Cat.prototype.get_name = function() {
+  return this.says() + ' ' + this.name + ' ' + this.says();
+};
+
+var myCat = new Cat('Henrietta');
+var says = myCat.says(); // meow
+var purr = myCat.purr(5); // r-r-r-r-r
+var name = myCat.get_name();
